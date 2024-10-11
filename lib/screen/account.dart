47e58152b69adeb967bf3c67904a37f8639ca8a1
qotjwns <qotjwns112@cau.ac.kr>
2024-10-11 12:36:26 +0900

    connect screen propriately

--- lib/screen/account.dart
+++ lib/screen/account.dart
@@ -6,107 +6,106 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Container(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: const Text(
-              'Account',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 50,
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: const Text(
+            'Account',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
             ),
           ),
         ),
-        body: SingleChildScrollView(  // ScrollView 추가
-          child: Padding(
-            padding: const EdgeInsets.only(top: 50.0),
-            child: Column(
-              children: <Widget>[
-                const Center(
-                  child: Text(
-                    'User Data',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      body: SingleChildScrollView(
+        // ScrollView 추가
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            children: <Widget>[
+              const Center(
+                child: Text(
+                  'User Data',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  buildUserStatus(
+                    label: 'Weight',
+                    imagePath: 'assets/images/weight.png',
+                    unitlabel: 'KG',
                   ),
-                ),
-                const SizedBox(height: 100),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    buildUserStatus(
-                      label: 'Weight',
-                      imagePath: 'assets/images/weight.png',
-                      unitlabel: 'KG',
-                    ),
-                    const SizedBox(width: 20),
-                    buildUserStatus(
-                      label: 'Muscle Mass',
-                      imagePath: 'assets/images/muscle mass.png',
-                      unitlabel: 'KG',
-                    ),
-                    const SizedBox(width: 20),
-                    buildUserStatus(
-                      label: 'Body Fat',
-                      imagePath: 'assets/images/body fat.png',
-                      unitlabel: '%',
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 50),
-                Row(mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                        children:[
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor : Colors.grey
-                              ),
-                              onPressed: (){
-                              },
-                              child: const Text(
-                                'Save',
-                                style: TextStyle(color: Colors.black,fontSize: 20, fontWeight: FontWeight.bold),
-                              )
-                          )
-                        ]
-                    )
-                  ],
-                ),
-                const SizedBox(height: 200),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Image.asset(
-                          'assets/images/exit.png',
-                          width: 50,
-                          height: 50,
+                  const SizedBox(width: 20),
+                  buildUserStatus(
+                    label: 'Muscle Mass',
+                    imagePath: 'assets/images/muscle mass.png',
+                    unitlabel: 'KG',
+                  ),
+                  const SizedBox(width: 20),
+                  buildUserStatus(
+                    label: 'Body Fat',
+                    imagePath: 'assets/images/body fat.png',
+                    unitlabel: '%',
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey),
+                        onPressed: () {},
+                        child: const Text(
+                          'Save',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ))
+                  ])
+                ],
+              ),
+              const SizedBox(height: 200),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/exit.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      const SizedBox(width: 20),
+                      GestureDetector(
+                        onDoubleTap: () {
+                          SystemNavigator.pop();
+                        },
+                        child: const Text(
+                          'Exit app',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
                         ),
-                        const SizedBox(width: 20),
-                        GestureDetector(
-                          onDoubleTap: (){
-                            SystemNavigator.pop();
-                          },
-                          child: const Text(
-                            'Exit app',
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        const SizedBox(width: 30),
-                        const Text(
-                          'Change Password',
-                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                      ),
+                      const SizedBox(width: 30),
+                      const Text(
+                        'Change Password',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
@@ -137,18 +136,18 @@ class AccountScreen extends StatelessWidget {
         NumberInputBox(labelText: unitlabel),
       ],
     );
-
   }
 }
 
 class NumberInputBox extends StatelessWidget {
   final String labelText;
+
   const NumberInputBox({super.key, required this.labelText});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,  // 정사각형 너비
+      width: 100, // 정사각형 너비
       height: 50, // 정사각형 높이
       decoration: BoxDecoration(
         color: Colors.white, // 박스 내부 색상
@@ -158,24 +157,22 @@ class NumberInputBox extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           const Expanded(
-            child : NumberInputField(),
+            child: NumberInputField(),
           ),
           Padding(
               padding: const EdgeInsets.only(right: 8.0),
               child: Text(
                 labelText,
                 style: const TextStyle(color: Colors.black, fontSize: 14),
-              )
-          )
-
-        ],// 숫자 입력 필드 추가
+              ))
+        ], // 숫자 입력 필드 추가
       ),
     );
   }
 }
+
 class NumberInputField extends StatelessWidget {
   const NumberInputField({super.key});
-
 
   @override
   Widget build(BuildContext context) {
